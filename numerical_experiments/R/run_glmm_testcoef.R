8e5c59887ab45ad53feaf2699ec3e629e93888bf
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-08-08 10:18:24 -0400

    update boot

--- numerical_experiments/R/run_glmm_testcoef.R
+++ numerical_experiments/R/run_glmm_testcoef.R
@@ -2,8 +2,10 @@
 
 #' Notes: 
 #' cd $ups 
+#' git pull
+#' 
 #' cd numerical_experiments/R
-#' Rnosave run_glmm_testcoef.R -t 1-1000 -tc 100 -N JOB_glmm
+#' Rnosave run_glmm_testcoef.R -t 1-1000 -tc 50 -N JOB_glmm
 
 
 arg_str <- as.character(Sys.getenv("SGE_TASK_ID"))
@@ -41,7 +43,7 @@ eff_tar_grid  <- c(0.5, 1)
 eff_tru       <- coef_x1
 
 # number of boot repetitions within one experiment, one setup
-B_boot  <- 2
+B_boot  <- 1000
 
 # simulate sample (for maximum sample size first)
 # subject-specific
