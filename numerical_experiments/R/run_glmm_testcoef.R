815a56bfba9202c553367088cb726976d3a5c418
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-08-10 17:54:59 -0400

    update glmm

--- numerical_experiments/R/run_glmm_testcoef.R
+++ numerical_experiments/R/run_glmm_testcoef.R
@@ -6,7 +6,12 @@
 #' 
 #' cd numerical_experiments/R
 #' Rnosave run_glmm_testcoef.R -t 1-1000 -tc 50 -N JOB_glmm
-#' Rnosave run_glmm_testcoef.R -t 1-1 -tc 50 -N JOB_glmm
+#' Rnosave run_glmm_testcoef.R -t 1-1 -tc 50 -N JOB_glmm0
+#' 
+#' ls -l -d *JOB_glmm0*
+#' rm JOB_onesample*
+#' 
+#' rm $ups/numerical_experiments/results_CL/2021-08-07-glmm_testcoef_raw/*
 
 
 arg_str <- as.character(Sys.getenv("SGE_TASK_ID"))
@@ -73,179 +78,18 @@ dat       <- data.frame(y = y_ij, x1 = x1_ij, x2 = x2_ij,  x3 = x3_ij,
 dat_subjid_x1_is1 <- unique(dat %>% filter(x1 == 1) %>% pull(subjid))
 dat_subjid_x1_is0 <- unique(dat %>% filter(x1 == 0) %>% pull(subjid))
 
-# get observed effect 
-fit_obs  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat, family = binomial)
-
 message(paste0("nrow(dat): ", nrow(dat)))
 message(paste0("mean(dat$y): ", round(mean(dat$y), 2)))
 message(paste0("fixef(fit_obs)['x1']: ", round(fixef(fit_obs)["x1"], 2)))
 
 
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-# RUN SIMULATIONS
-
 t1 <- Sys.time()
 mat_out_all <- data.frame()
 
-  
-# ------------------------------------------------------------------------------
-# ESTIMATE POWER WITH upstrap, for observed effect size 
-
-# define object to store values across B resamplings
-mat_boot <- matrix(NA, nrow = N_tar_grid_l, ncol = B_boot)
-##
-for (bb in 1 : B_boot){ # bb <- 1; rr <- 1
-  print(bb)
-  if (bb %% 100 == 0) message(paste0("bb: ", bb, " [", round(bb / B_boot * 100, 2), "%], ",  round(as.numeric(Sys.time() - t1, unit = "mins")), " mins"))
-  # resample data indices for current boot repetition (upstrap up to N target max)
-  dat_bb_idx_x1_is1 <- sample(x = dat_subjid_x1_is1, size = N_tar_max, replace = TRUE)
-  dat_bb_idx_x1_is0 <- sample(x = dat_subjid_x1_is0, size = N_tar_max, replace = TRUE)
-  dat_bb_x1_is1     <- lapply(dat_bb_idx_x1_is1, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
-  dat_bb_x1_is0     <- lapply(dat_bb_idx_x1_is0, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
-  ## make new subj ID so as to treat resampled subjects as new ones
-  dat_bb_x1_is1$subjid <- rep(1 : N_tar_max, each = ni)
-  dat_bb_x1_is0$subjid <- rep((N_tar_max + 1) : (2 * N_tar_max), each = ni)
-  dat_bb_x1_is1$subjid_arm <- rep(1 : N_tar_max, each = ni)
-  dat_bb_x1_is0$subjid_arm <- rep(1 : N_tar_max, each = ni)
-  # length(unique(c(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjid)))
-  # intersect(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjidy)
-  dat_bb <- rbind(dat_bb_x1_is1, dat_bb_x1_is0)
-  for (rr in 1 : N_tar_grid_l){ 
-    tryCatch({
-      N_tar      <- N_tar_grid[rr]
-      dat_bb_rr  <- dat_bb[dat_bb$subjid_arm <= N_tar, ]
-      fit_bb_rr  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat_bb_rr, family = binomial)  
-      pval_bb_rr <- summary(fit_bb_rr)$coef["x1", 4]
-      mat_boot[rr, bb] <- (pval_bb_rr < 0.05) * 1
-    }, error = function(e) {message(e)})
-  }
-}
-value <- rowMeans(mat_boot, na.rm = TRUE)
-##
-mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
-mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
-mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
-mat_out_tmp$name          <- "upstrap_power"
-mat_out_tmp$eff_tru       <- eff_tru
-mat_out_tmp$eff_tar       <- NA # observed power case
-mat_out_tmp$value         <- value
-mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
-rm(mat_out_tmp, value, mat_boot)
-
-
-
-# ------------------------------------------------------------------------------
-# ESTIMATE POWER WITH upstrap, for fixed target effect size 
-
-for (eff_tar in eff_tar_grid){ # eff_tar <- eff_tar_grid[1]
-  message(paste0("eff_tar = ", eff_tar))
-  # update 
-  fit_obs_upd <- fit_obs
-  fixef(fit_obs_upd)["x1"] <- eff_tar
-  # define object to store values across B resamplings
-  mat_boot <- matrix(NA, nrow = N_tar_grid_l, ncol = B_boot)
-  ##
-  for (bb in 1 : B_boot){ # bb <- 1; rr <- 1
-    print(bb)
-    if (bb %% 100 == 0) message(paste0("bb: ", bb, " [", round(bb / B_boot * 100, 2), "%], ",  round(as.numeric(Sys.time() - t1, unit = "mins")), " mins"))
-    # resample data indices for current boot repetition (upstrap up to N target max)
-    dat_bb_idx_x1_is1 <- sample(x = dat_subjid_x1_is1, size = N_tar_max, replace = TRUE)
-    dat_bb_idx_x1_is0 <- sample(x = dat_subjid_x1_is0, size = N_tar_max, replace = TRUE)
-    dat_bb_x1_is1     <- lapply(dat_bb_idx_x1_is1, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
-    dat_bb_x1_is0     <- lapply(dat_bb_idx_x1_is0, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
-    # simulate new Y 
-    dat_bb_x1_is1$y <- simulate(fit_obs_upd, nsim = 1, newdata = dat_bb_x1_is1)[[1]]
-    dat_bb_x1_is0$y <- simulate(fit_obs_upd, nsim = 1, newdata = dat_bb_x1_is0)[[1]]
-    ## make new subj ID so as to treat resampled subjects as new ones
-    dat_bb_x1_is1$subjid <- rep(1 : N_tar_max, each = ni)
-    dat_bb_x1_is0$subjid <- rep((N_tar_max + 1) : (2 * N_tar_max), each = ni)
-    dat_bb_x1_is1$subjid_arm <- rep(1 : N_tar_max, each = ni)
-    dat_bb_x1_is0$subjid_arm <- rep(1 : N_tar_max, each = ni)
-    # length(unique(c(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjid)))
-    # intersect(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjidy)
-    dat_bb <- rbind(dat_bb_x1_is1, dat_bb_x1_is0)
-    for (rr in 1 : N_tar_grid_l){ 
-      tryCatch({
-        N_tar      <- N_tar_grid[rr]
-        dat_bb_rr  <- dat_bb[dat_bb$subjid_arm <= N_tar, ]
-        fit_bb_rr  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat_bb_rr, family = binomial)  
-        pval_bb_rr <- summary(fit_bb_rr)$coef["x1", 4]
-        mat_boot[rr, bb] <- (pval_bb_rr < 0.05) * 1
-      }, error = function(e) {message(e)})
-    }
-  }
-  value <- rowMeans(mat_boot, na.rm = TRUE)
-  ##
-  mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
-  mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
-  mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
-  mat_out_tmp$name          <- "upstrap_power"
-  mat_out_tmp$eff_tru       <- eff_tru
-  mat_out_tmp$eff_tar       <- eff_tar # observed power case
-  mat_out_tmp$value         <- value
-  mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
-  rm(mat_out_tmp, value, mat_boot)
-}
-
-
-
-# ------------------------------------------------------------------------------
-# ESTIMATE POWER WITH simr, for observed effect size 
-
-# define simr-specific grid of sample size 
-# (represents total number of subjects, not: number subjects in each of the "treatment arms")
-N_tar_grid_simr <- N_tar_grid * 2 
-
-fit_obs_simr  <- fit_obs
-fit_ext_simr  <- simr::extend(fit_obs_simr, along = "subjid", n = max(N_tar_grid_simr))
-pc_out        <- simr::powerCurve(fit_ext_simr, along = "subjid", breaks = N_tar_grid_simr, nsim = B_boot, progress = FALSE)
-pc_out_s      <- summary(pc_out)
-value         <- pc_out_s$mean
-##
-mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
-mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
-mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
-mat_out_tmp$name          <- "simr_power"
-mat_out_tmp$eff_tru       <- eff_tru
-mat_out_tmp$eff_tar       <- NA # observed power case
-mat_out_tmp$value         <- value
-mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
-rm(mat_out_tmp, value, fit_ext, pc_out, pc_out_s)
-
-
-# ------------------------------------------------------------------------------
-# ESTIMATE POWER WITH simr, for fixed target effect size 
-
-for (eff_tar in eff_tar_grid){ 
-  message(paste0("SIMR -- eff_tar = ", eff_tar))
-  fit_obs_simr <- fit_obs
-  # update effect size
-  fixef(fit_obs_simr)["x1"] <- eff_tar
-  fit_ext_simr  <- simr::extend(fit_obs_simr, along = "subjid", n = max(N_tar_grid_simr))
-  pc_out        <- simr::powerCurve(fit_ext_simr, along = "subjid", breaks = N_tar_grid_simr, nsim = B_boot, progress = FALSE)
-  pc_out_s      <- summary(pc_out)
-  value         <- pc_out_s$mean
-  ##
-  mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
-  mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
-  mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
-  mat_out_tmp$name          <- "simr_power"
-  mat_out_tmp$eff_tru       <- eff_tru
-  mat_out_tmp$eff_tar       <- eff_tar
-  mat_out_tmp$value         <- value
-  mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
-  rm(mat_out_tmp, value, fit_obs_simr, fit_ext_simr, pc_out, pc_out_s)
-}
-
-
-
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# ADDITIONAL SIMULATIONS TO BE RUN ONCE 
-
+# RUN SIMULATIONS TO BE RUN ONCE
 
 if (arrayjob_idx == 1){
   
@@ -302,16 +146,180 @@ if (arrayjob_idx == 1){
   }
 }
 
+out_fpath_raw <- paste0(res_fdir_raw, "/arrayjob_", arrayjob_idx, ".rds")
+saveRDS(object = mat_out_all, file = out_fpath_raw)
 
-t2 <- Sys.time()
-t_diff <- round(as.numeric(t2 - t1, unit = "mins"), 1)
-message(paste0("COMPLETED. Time difference of ", t_diff, " minutes."))
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# RUN OTHER SIMULATIONS
+
+  
+# ------------------------------------------------------------------------------
+# ESTIMATE POWER WITH upstrap, for observed effect size 
+
+# define object to store values across B resamplings
+mat_boot <- matrix(NA, nrow = N_tar_grid_l, ncol = B_boot)
+##
+for (bb in 1 : B_boot){ # bb <- 1; rr <- 1
+  print(bb)
+  if (bb %% 100 == 0) message(paste0("bb: ", bb, " [", round(bb / B_boot * 100, 2), "%], ",  round(as.numeric(Sys.time() - t1, unit = "mins")), " mins"))
+  # resample data indices for current boot repetition (upstrap up to N target max)
+  dat_bb_idx_x1_is1 <- sample(x = dat_subjid_x1_is1, size = N_tar_max, replace = TRUE)
+  dat_bb_idx_x1_is0 <- sample(x = dat_subjid_x1_is0, size = N_tar_max, replace = TRUE)
+  dat_bb_x1_is1     <- lapply(dat_bb_idx_x1_is1, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
+  dat_bb_x1_is0     <- lapply(dat_bb_idx_x1_is0, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
+  ## make new subj ID so as to treat resampled subjects as new ones
+  dat_bb_x1_is1$subjid <- rep(1 : N_tar_max, each = ni)
+  dat_bb_x1_is0$subjid <- rep((N_tar_max + 1) : (2 * N_tar_max), each = ni)
+  dat_bb_x1_is1$subjid_arm <- rep(1 : N_tar_max, each = ni)
+  dat_bb_x1_is0$subjid_arm <- rep(1 : N_tar_max, each = ni)
+  # length(unique(c(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjid)))
+  # intersect(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjidy)
+  dat_bb <- rbind(dat_bb_x1_is1, dat_bb_x1_is0)
+  for (rr in 1 : N_tar_grid_l){ 
+    tryCatch({
+      N_tar      <- N_tar_grid[rr]
+      dat_bb_rr  <- dat_bb[dat_bb$subjid_arm <= N_tar, ]
+      fit_bb_rr  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat_bb_rr, family = binomial)  
+      pval_bb_rr <- summary(fit_bb_rr)$coef["x1", 4]
+      mat_boot[rr, bb] <- (pval_bb_rr < 0.05) * 1
+    }, error = function(e) {message(e)})
+  }
+}
+value <- rowMeans(mat_boot, na.rm = TRUE)
+##
+mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
+mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
+mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
+mat_out_tmp$name          <- "upstrap_power"
+mat_out_tmp$eff_tru       <- eff_tru
+mat_out_tmp$eff_tar       <- NA # observed power case
+mat_out_tmp$value         <- value
+mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
+rm(mat_out_tmp, value, mat_boot)
+
+
+
+# ------------------------------------------------------------------------------
+# ESTIMATE POWER WITH upstrap, for fixed target effect size 
+
+for (eff_tar in eff_tar_grid){ # eff_tar <- eff_tar_grid[1]
+  message(paste0("eff_tar = ", eff_tar))
+  # define object to store values across B resamplings
+  mat_boot <- matrix(NA, nrow = N_tar_grid_l, ncol = B_boot)
+  ##
+  for (bb in 1 : B_boot){ # bb <- 1; rr <- 1
+    print(bb)
+    if (bb %% 100 == 0) message(paste0("bb: ", bb, " [", round(bb / B_boot * 100, 2), "%], ",  round(as.numeric(Sys.time() - t1, unit = "mins")), " mins"))
+    # resample data indices for current boot repetition (upstrap up to N target max)
+    dat_bb_idx_x1_is1 <- sample(x = dat_subjid_x1_is1, size = N_tar_max, replace = TRUE)
+    dat_bb_idx_x1_is0 <- sample(x = dat_subjid_x1_is0, size = N_tar_max, replace = TRUE)
+    dat_bb_x1_is1     <- lapply(dat_bb_idx_x1_is1, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
+    dat_bb_x1_is0     <- lapply(dat_bb_idx_x1_is0, function(subjid_tmp) dat %>% filter(subjid == subjid_tmp)) %>% do.call("rbind", .)
+    ## make new subj ID so as to treat resampled subjects as new ones
+    dat_bb_x1_is1$subjid <- rep(1 : N_tar_max, each = ni)
+    dat_bb_x1_is0$subjid <- rep((N_tar_max + 1) : (2 * N_tar_max), each = ni)
+    dat_bb_x1_is1$subjid_arm <- rep(1 : N_tar_max, each = ni)
+    dat_bb_x1_is0$subjid_arm <- rep(1 : N_tar_max, each = ni)
+    # length(unique(c(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjid)))
+    # intersect(dat_bb_x1_is1$subjid, dat_bb_x1_is0$subjidy)
+    dat_bb <- rbind(dat_bb_x1_is1, dat_bb_x1_is0)
+    for (rr in 1 : N_tar_grid_l){ 
+      tryCatch({
+        N_tar      <- N_tar_grid[rr]
+        dat_bb_rr  <- dat_bb[dat_bb$subjid_arm <= N_tar, ]
+        # estimate fit observed, fit updated
+        fit_obs  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat_bb_rr, family = binomial)
+        fit_obs_upd <- fit_obs
+        fixef(fit_obs_upd)["x1"] <- eff_tar
+        # simulate new Y 
+        dat_bb_rr$y <- simulate(fit_obs_upd, nsim = 1, newdata = dat_bb_rr)[[1]]
+        # refit 
+        fit_bb_rr  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat_bb_rr, family = binomial)  
+        pval_bb_rr <- summary(fit_bb_rr)$coef["x1", 4]
+        mat_boot[rr, bb] <- (pval_bb_rr < 0.05) * 1
+      }, error = function(e) {message(e)})
+    }
+  }
+  value <- rowMeans(mat_boot, na.rm = TRUE)
+  ##
+  mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
+  mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
+  mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
+  mat_out_tmp$name          <- "upstrap_power"
+  mat_out_tmp$eff_tru       <- eff_tru
+  mat_out_tmp$eff_tar       <- eff_tar # observed power case
+  mat_out_tmp$value         <- value
+  mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
+  rm(mat_out_tmp, value, mat_boot)
+}
+
+
+# ------------------------------------------------------------------------------
+# ESTIMATE POWER WITH simr, for observed effect size 
+
+# get observed effect 
+fit_obs  <- glmer(y ~ x1 + x2 + x3 + (1 | subjid), data = dat, family = binomial)
+
+# define simr-specific grid of sample size 
+# (represents total number of subjects, not: number subjects in each of the "treatment arms")
+N_tar_grid_simr <- N_tar_grid * 2 
+
+fit_obs_simr  <- fit_obs
+fit_ext_simr  <- simr::extend(fit_obs_simr, along = "subjid", n = max(N_tar_grid_simr))
+pc_out        <- simr::powerCurve(fit_ext_simr, along = "subjid", breaks = N_tar_grid_simr, nsim = B_boot, progress = FALSE)
+pc_out_s      <- summary(pc_out)
+value         <- pc_out_s$mean
+##
+mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
+mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
+mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
+mat_out_tmp$name          <- "simr_power"
+mat_out_tmp$eff_tru       <- eff_tru
+mat_out_tmp$eff_tar       <- NA # observed power case
+mat_out_tmp$value         <- value
+mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
+rm(mat_out_tmp, value, fit_ext, pc_out, pc_out_s)
+
+
+
+# ------------------------------------------------------------------------------
+# ESTIMATE POWER WITH simr, for fixed target effect size 
+
+for (eff_tar in eff_tar_grid){ 
+  message(paste0("SIMR -- eff_tar = ", eff_tar))
+  fit_obs_simr <- fit_obs
+  # update effect size
+  fixef(fit_obs_simr)["x1"] <- eff_tar
+  fit_ext_simr  <- simr::extend(fit_obs_simr, along = "subjid", n = max(N_tar_grid_simr))
+  pc_out        <- simr::powerCurve(fit_ext_simr, along = "subjid", breaks = N_tar_grid_simr, nsim = B_boot, progress = FALSE)
+  pc_out_s      <- summary(pc_out)
+  value         <- pc_out_s$mean
+  ##
+  mat_out_tmp               <- data.frame(N_tar = N_tar_grid)
+  mat_out_tmp$N_obs         <- rep(N_obs, N_tar_grid_l)
+  mat_out_tmp$arrayjob_idx  <- rep(arrayjob_idx, N_tar_grid_l)
+  mat_out_tmp$name          <- "simr_power"
+  mat_out_tmp$eff_tru       <- eff_tru
+  mat_out_tmp$eff_tar       <- eff_tar
+  mat_out_tmp$value         <- value
+  mat_out_all               <- rbind(mat_out_all, mat_out_tmp)
+  rm(mat_out_tmp, value, fit_obs_simr, fit_ext_simr, pc_out, pc_out_s)
+}
+
 
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # SAVE TO FILE 
+
+t2 <- Sys.time()
+t_diff <- round(as.numeric(t2 - t1, unit = "mins"), 1)
+message(paste0("COMPLETED. Time difference of ", t_diff, " minutes."))
 
 out_fpath_raw <- paste0(res_fdir_raw, "/arrayjob_", arrayjob_idx, ".rds")
 saveRDS(object = mat_out_all, file = out_fpath_raw)
